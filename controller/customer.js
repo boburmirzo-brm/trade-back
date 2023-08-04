ecbbeb5db7fcd62a7a5a322eb11eb27cc2b07aec
n1stodev <nurillotojiakhmedov@gmail.com>
2023-08-04 12:23:17 +0500

    customer and payment

---
+++ controller/customer.js
@@ -0,0 +1,39 @@
+const { Customers, validateCustomer } = require("../model/customerSchema");
+
+exports.getCustomer = async (req, res) => {
+    try {
+        const customers = await Customers.find();
+        res
+            .status(200)
+            .json({ variant: "success", msg: "Barcha mijozlar", innerData: customers });
+    } catch {
+        res
+            .status(500)
+            .json({ variant: "error", msg: "server error", innerData: null });
+    }
+};
+
+exports.createCustomer = async (req, res) => {
+    try {
+        const { error } = validateCustomer(req.body);
+        if (error) {
+            return res.status(400).json({
+                variant: "warning",
+                msg: error.details[0].message,
+                innerData: null,
+            });
+        }
+        const newCustomer = await Customers.create(req.body);
+        res.status(201).json({
+            variant: "success",
+            msg: "Mijoz muvaffaqiyatli qo'shildi",
+            innerData: newCustomer,
+        });
+    } catch {
+        res.status(500).json({
+            variant: "error",
+            msg: "server error",
+            innerData: null,
+        });
+    }
+};
--- router/index.js
+++ router/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
-const {getAdmins, createAdmin} = require("../controller/admin")
+const { getAdmins, createAdmin } = require("../controller/admin")
+const { getPayments, createPayment } = require("../controller/payment")
+const { getCustomer, createCustomer } = require("../controller/customer")
 
 // get -> get
 // delete -> delete
@@ -11,6 +13,14 @@ const {getAdmins, createAdmin} = require("../controller/admin")
 // Admin route
 router.get("/get/admin", getAdmins)
 router.post("/create/admin", createAdmin)
+
+// Payment route
+router.get("/get/payment", getPayments)
+router.post("/create/payment", createPayment)
+
+// Customer route
+router.get("/get/customer", getCustomer)
+router.post("/create/customer", createCustomer)
 
 
 // Order route
