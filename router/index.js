340508b21de7b93f37fc2b467691081857bc2fbb
Jurabek Abduhalilov <jurabekabduhalilov105@gmail.com>
2023-08-08 10:30:46 +0500

    Added Create & Delete. Status of input and output now works correctly

--- router/index.js
+++ router/index.js
@@ -1,14 +1,23 @@
-const express = require("express")
-const router = express.Router()
-const {getSellers, createSeller} = require("../controller/seller")
-const { getBuyOrSells } = require("../controller/buyOrSell")
-const { getAdmins, createAdmin } = require("../controller/admin")
-const { getSalary, createSalary, updateSalary, deleteSalary } = require("../controller/salary")
-const { getProducts, createProduct } = require("../controller/product");
-const { getPayments, createPayment } = require("../controller/payment")
-const { getCustomer, createCustomer } = require("../controller/customer")
-const { getOrders, createOrder } = require("../controller/order")
-const { getExpenses, createExpense } = require("../controller/expense")
+const express = require('express');
+const router = express.Router();
+const { getSellers, createSeller } = require('../controller/seller');
+const {
+  getBuyOrSells,
+  createBuyOrSell,
+  deleteBuyOrSell,
+} = require('../controller/buyOrSell');
+const { getAdmins, createAdmin } = require('../controller/admin');
+const {
+  getSalary,
+  createSalary,
+  updateSalary,
+  deleteSalary,
+} = require('../controller/salary');
+const { getProducts, createProduct } = require('../controller/product');
+const { getPayments, createPayment } = require('../controller/payment');
+const { getCustomer, createCustomer } = require('../controller/customer');
+const { getOrders, createOrder } = require('../controller/order');
+const { getExpenses, createExpense } = require('../controller/expense');
 
 // get -> get
 // delete -> delete
@@ -17,46 +26,43 @@ const { getExpenses, createExpense } = require("../controller/expense")
 // post -> create
 
 // Admin route
-router.get("/get/admin", getAdmins)
-router.post("/create/admin", createAdmin)
+router.get('/get/admin', getAdmins);
+router.post('/create/admin', createAdmin);
 
 // BuyOrSell route
-router.get("/get/buy-or-sell", getBuyOrSells)
-
+router.get('/get/buy-or-sell', getBuyOrSells);
+router.post('/create/buy-or-sell', createBuyOrSell);
+router.delete('/delete/buy-or-sell/:id', deleteBuyOrSell);
 
 // Order route
-router.get("/get/orders", getOrders)
-router.post("/create/order", createOrder)
+router.get('/get/orders', getOrders);
+router.post('/create/order', createOrder);
 
 // Expense route
-router.get("/get/expenses", getExpenses)
-router.post("/create/expense", createExpense)
-
-
+router.get('/get/expenses', getExpenses);
+router.post('/create/expense', createExpense);
 
 // Seller route
-router.get("/get/seller", getSellers)
-router.post("/create/seller", createSeller)
+router.get('/get/seller', getSellers);
+router.post('/create/seller', createSeller);
 
 // Salary route
-router.get("/get/salary", getSalary)
-router.post("/create/salary", createSalary)
-router.put("/update/salary", updateSalary)
-router.delete("/delete/salary", deleteSalary)
+router.get('/get/salary', getSalary);
+router.post('/create/salary', createSalary);
+router.put('/update/salary', updateSalary);
+router.delete('/delete/salary', deleteSalary);
 // Order route
 
-
 //? product route
-router.get("/get/product", getProducts);
-router.post("/create/product", createProduct);
+router.get('/get/product', getProducts);
+router.post('/create/product', createProduct);
 
 // Payment route
-router.get("/get/payment", getPayments)
-router.post("/create/payment", createPayment)
+router.get('/get/payment', getPayments);
+router.post('/create/payment', createPayment);
 
 // Customer route
-router.get("/get/customer", getCustomer)
-router.post("/create/customer", createCustomer)
+router.get('/get/customer', getCustomer);
+router.post('/create/customer', createCustomer);
 
-
-module.exports = router
+module.exports = router;
